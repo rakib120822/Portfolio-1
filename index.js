0f8232bf92c06e382712e379f661fe616ef7410e
rakib120822 <payelrakibulislam@gmail.com>
2025-09-02 00:03:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const typed = new Typed(".titleTag", {
+  strings: ["Web Developer", "Frontend Developer", "PME Engineer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+
+  loop: true,
+});
